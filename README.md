2fbe2901ef1191e0c5e99ca7b6914796ecbc4a75
Shanavas Shaji <shanavascruise@gmail.com>
2023-12-08 15:07:02 +0200

    Inherited Widget example

--- README.md
+++ README.md
@@ -6,8 +6,12 @@ This project is aimed at learning flutter using examples
 
 Add a new configuration with one of the files listed below as entrypoint if you want to run it.
 
-1. provider.dart - A simple example shows how to use the Provider Consumer state management in flutter
-                    This needs the Provider package and make sure it is already added to your project using the command
-                    ```flutter pub add provider```
-2. multiple_providers.dart - This example shows how to use multiple Providers and how to consume multiple providers.
+1. inherited_widget.dart - This example shows how to use an inherited widget to avoid passing data down to each child widgets.
+2. provider.dart - This example shows how to use the Provider Consumer state management.
+                   Advantages compared to Inherited Widget is that you don't need to setup a separate Stateful widget class for class.
+                   Also no need to use InitState and SetState methods for state update.
+                   This needs the Provider package and make sure it is already added to your project using the command 
+                   ```flutter pub add provider```
+3. multiple_providers.dart - This example shows how to use multiple Providers and how to consume multiple providers.
+                             Similar advantages as the previous example.
                             Make sure to install the provider package as in the previous example.
---
+++ lib/inherited_widget.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: MyHomePage(),
+    ),
+  );
+}
+
+class CounterInheritedWidget extends InheritedWidget {
+  final int counter;
+  final VoidCallback increment;
+  final Widget childWidget;
+
+  const CounterInheritedWidget({super.key,
+    required this.counter,
+    required this.increment,
+    required this.childWidget,
+  }) : super(child: childWidget);
+
+  @override
+  bool updateShouldNotify(CounterInheritedWidget oldWidget) {
+    return counter != oldWidget.counter;
+  }
+
+  static CounterInheritedWidget? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<CounterInheritedWidget>();
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() {
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (kDebugMode) {
+      print('rebuild MyHomePage');
+    }
+
+    return Scaffold(
+      body: CounterInheritedWidget(
+          counter: _counter,
+          increment: increment,
+          childWidget: Builder(
+            builder: (BuildContext innerContext) {
+              return const MyCenterWidget();
+              },
+          )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: increment,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class MyCenterWidget extends StatelessWidget {
+  const MyCenterWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    if (kDebugMode) {
+      print('rebuild MyCenterWidget');
+    }
+    return const Center(
+      child: CounterWidget(),
+    );
+  }
+}
+
+class CounterWidget extends StatelessWidget {
+  const CounterWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    if (kDebugMode) {
+      print('rebuild CounterWidget');
+    }
+    final myInheritedWidget = CounterInheritedWidget.of(context);
+    return Text(
+          '${myInheritedWidget?.counter}'
+        );
+  }
+}
